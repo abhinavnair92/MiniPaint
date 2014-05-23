2ea73fa51edace723b4675089ba5813600dd2662
AlbertYuChen <chenyu610103@gmail.com>
2014-05-23 12:58:07 -0400

    Rectangular & Trangle

--- Assignment1_minipaint/Test.cpp
+++ Assignment1_minipaint/Test.cpp
@@ -117,14 +117,14 @@ void display(void){
     // Print
     glFlush();
 }
-
-int main(int argc,char *argv[]){
-    glutInit(&argc,argv);
-    glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
-    glutInitWindowSize(400,400);
-    glutInitWindowPosition(100,100);
-    glutCreateWindow("Albert Polygon Clipping!");
-    glutDisplayFunc(display);
-    glutMainLoop();
-    return 0;
-}
+//
+//int main(int argc,char *argv[]){
+//    glutInit(&argc,argv);
+//    glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
+//    glutInitWindowSize(400,400);
+//    glutInitWindowPosition(100,100);
+//    glutCreateWindow("Albert Polygon Clipping!");
+//    glutDisplayFunc(display);
+//    glutMainLoop();
+//    return 0;
+//}
